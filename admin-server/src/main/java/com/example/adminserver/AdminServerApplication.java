061776f90e8327efc6df320aaa4efdea68bbc111
wangqi <wangqi@wind56.com>
2019-02-20 18:53:10 +0800

    尝试解决 java.lang.IllegalStateException: Calling [asyncError()] is not valid for a request with Async state [MUST_DISPATCH] https://stackoverflow.com/questions/53605197/calling-asyncerror-is-not-valid-for-a-request-with-async-state-must-dispatc

--- admin-server/src/main/java/com/example/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/example/adminserver/AdminServerApplication.java
@@ -2,14 +2,19 @@ package com.example.adminserver;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 @EnableAdminServer
 public class AdminServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdminServerApplication.class, args);
+//        SpringApplication.run(AdminServerApplication.class, args);
+        new SpringApplicationBuilder(AdminServerApplication.class)
+                .web(WebApplicationType.REACTIVE)
+                .run(args);
     }
 
 }
