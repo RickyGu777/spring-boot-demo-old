955b8b0a753bbb7819e3d52517b40ae8f2f01cc8
wangqi <wangqi@wind56.com>
2019-02-20 17:02:49 +0800

    更换端口测试服务器是否能正常启动

--- service-feign/src/main/java/com/example/servicefeign/Controller/HiController.java
+++ service-feign/src/main/java/com/example/servicefeign/Controller/HiController.java
@@ -1,6 +1,7 @@
 package com.example.servicefeign.Controller;
 
 import com.example.servicefeign.Service.SchedualServiceHi;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
+@AllArgsConstructor
 @Slf4j
 public class HiController {
 
     //编译器报错，无视。 因为这个Bean是在程序启动的时候注入的，编译器感知不到，所以报错。
-    @Autowired
-    SchedualServiceHi schedualServiceHi;
+    private final SchedualServiceHi schedualServiceHi;
 
     @GetMapping(value = "/hi")
     public String sayHi(@RequestParam String name) {
