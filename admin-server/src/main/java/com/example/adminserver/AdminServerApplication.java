dead37bf48189ec057cb0b141eb8567da5d5e2f5
wangqi <wangqi@wind56.com>
2019-02-20 17:48:14 +0800

    修改application.name大小写,取消admin-server在eureka注册

--- admin-server/src/main/java/com/example/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/example/adminserver/AdminServerApplication.java
@@ -3,11 +3,9 @@ package com.example.adminserver;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableAdminServer
-@EnableEurekaClient
 public class AdminServerApplication {
 
     public static void main(String[] args) {
