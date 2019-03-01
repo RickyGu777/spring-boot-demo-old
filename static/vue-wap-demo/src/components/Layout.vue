9e7bf3e90b195c8d2294c11b40f347ddebe45def
wangqi <wangqi@wind56.com>
2019-03-01 13:39:47 +0800

    富文本编辑器 vue-quill-editor

--- static/vue-wap-demo/src/components/Layout.vue
+++ static/vue-wap-demo/src/components/Layout.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="back_img">
-    <img src="../../static/GIF/sanjiu.gif">
+  <div>
+    <!--<img src="../../static/GIF/sanjiu.gif">-->
     <!--<video src="../../static/GIF/QQ视频20190123163248.mp4"/>-->
     <h1>{{ msg }}</h1>
+    <quillEditor v-model="msg"></quillEditor>
+    <!--<quill-editor ref="myTextEditor"-->
+                  <!--v-model="msg"-->
+                  <!--:config="editorOption"-->
+                  <!--@blur="onEditorBlur($event)"-->
+                  <!--@focus="onEditorFocus($event)"-->
+                  <!--@ready="onEditorReady($event)">-->
+    <!--</quill-editor>-->
   </div>
 </template>
 
 <script>
+  import {quillEditor} from 'vue-quill-editor'
+
   export default {
-    components: {},
+    components: {
+      quillEditor
+    },
     data() {
       return {
         msg: 'Welcome to Your Vue.js App'
--- static/vue-wap-demo/src/main.js
+++ static/vue-wap-demo/src/main.js
@@ -4,9 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './style/index.scss';
-// import './utils/rem'
+import VueQuillEditor from 'vue-quill-editor'
+
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueQuillEditor)
 
 /* eslint-disable no-new */
 new Vue({
