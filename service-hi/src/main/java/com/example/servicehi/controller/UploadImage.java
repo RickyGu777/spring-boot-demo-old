48153dd99bedcdecd89705118d5a8b8e6b4096ed
wangqi <wangqi@wind56.com>
2019-03-04 13:36:44 +0800

    上传图片到图床sm.ms

--- service-hi/src/main/java/com/example/servicehi/controller/UploadImage.java
+++ service-hi/src/main/java/com/example/servicehi/controller/UploadImage.java
@@ -1,5 +1,7 @@
 package com.example.servicehi.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.util.CompressImageUtil;
 import com.util.PostImg;
 import org.springframework.web.bind.annotation.*;
@@ -17,14 +19,16 @@ public class UploadImage {
         String compress = CompressImageUtil.compress(multipartFile);
 
         Map<String, String> fileMap = new HashMap<>();
-        fileMap.put("file", compress);
-        Map<String, String> textMap = new HashMap<String, String>();
-        textMap.put("smfile", multipartFile.getOriginalFilename());
+        fileMap.put("smfile", compress);
+        Map<String, String> textMap = new HashMap<>();
+        textMap.put("file_id", compress);
 
-        PostImg.formUpload(textMap, fileMap);
+        String s = PostImg.formUpload("https://sm.ms/api/upload", textMap, fileMap);
 
         HashMap<Object, Object> hashMap = new HashMap<>();
-        hashMap.put("file", compress);
+        Map map = JSON.parseObject(s, new TypeReference<Map>() {
+        });
+        hashMap.put("img", ((Map)map.get("data")).get("url"));
         return hashMap;
     }
 }
--- tool/src/main/java/com/util/PostImg.java
+++ tool/src/main/java/com/util/PostImg.java
@@ -8,13 +8,12 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class PostImg {
-    public static String formUpload(Map<String, String> textMap, Map<String, String> fileMap) {
+    public static String formUpload(String urlStr, Map<String, String> textMap, Map<String, String> fileMap) {
         String res = "";
-        String uploadUrl = "https://sm.ms/api/upload";
         HttpURLConnection conn = null;
-        String BOUNDARY = "---------------------------123821742118716"; //boundary就是request头和上传文件内容的分隔符
+        String BOUNDARY = "----WebKitFormBoundarypAIqI1RWBfPWiOKq"; //boundary就是request头和上传文件内容的分隔符
         try {
-            URL url = new URL(uploadUrl);
+            URL url = new URL(urlStr);
             conn = (HttpURLConnection) url.openConnection();
             conn.setConnectTimeout(5000);
             conn.setReadTimeout(30000);
@@ -30,25 +29,6 @@ public class PostImg {
                     "multipart/form-data; boundary=" + BOUNDARY);
 
             OutputStream out = new DataOutputStream(conn.getOutputStream());
-            // text
-            if (textMap != null) {
-                StringBuffer strBuf = new StringBuffer();
-                Iterator iter = textMap.entrySet().iterator();
-                while (iter.hasNext()) {
-                    Map.Entry entry = (Map.Entry) iter.next();
-                    String inputName = (String) entry.getKey();
-                    String inputValue = (String) entry.getValue();
-                    if (inputValue == null) {
-                        continue;
-                    }
-                    strBuf.append("\r\n").append("--").append(BOUNDARY).append(
-                            "\r\n");
-                    strBuf.append("Content-Disposition: form-data; name=\""
-                            + inputName + "\"\r\n\r\n");
-                    strBuf.append(inputValue);
-                }
-                out.write(strBuf.toString().getBytes());
-            }
 
             // file
             if (fileMap != null) {
@@ -67,8 +47,8 @@ public class PostImg {
                     if (filename.endsWith(".png")) {
                         contentType = "image/png";
                     }
-                    if (contentType == null || contentType.equals("")) {
-                        contentType = "application/octet-stream";
+                    if (filename.endsWith(".jpg") || filename.endsWith(".jpeg")) {
+                        contentType = "image/jpeg";
                     }
 
                     StringBuffer strBuf = new StringBuffer();
@@ -81,6 +61,34 @@ public class PostImg {
 
                     out.write(strBuf.toString().getBytes());
 
+//                    DataInputStream in = new DataInputStream(
+//                            new FileInputStream(file));
+//                    int bytes = 0;
+//                    byte[] bufferOut = new byte[1024];
+//                    while ((bytes = in.read(bufferOut)) != -1) {
+//                        out.write(bufferOut, 0, bytes);
+//                    }
+//                    in.close();
+                }
+            }
+
+            // text
+            if (textMap != null) {
+                StringBuffer strBuf = new StringBuffer();
+                Iterator iter = textMap.entrySet().iterator();
+                while (iter.hasNext()) {
+                    Map.Entry entry = (Map.Entry) iter.next();
+                    String inputName = (String) entry.getKey();
+                    String inputValue = (String) entry.getValue();
+                    if (inputValue == null) {
+                        continue;
+                    }
+                    strBuf.append("\r\n").append("--").append(BOUNDARY).append(
+                            "\r\n");
+                    strBuf.append("Content-Disposition: form-data; name=\""
+                            + inputName + "\"\r\n\r\n");
+
+                    File file = new File(inputValue);
                     DataInputStream in = new DataInputStream(
                             new FileInputStream(file));
                     int bytes = 0;
@@ -90,6 +98,7 @@ public class PostImg {
                     }
                     in.close();
                 }
+                out.write(strBuf.toString().getBytes());
             }
 
             byte[] endData = ("\r\n--" + BOUNDARY + "--\r\n").getBytes();
@@ -109,7 +118,7 @@ public class PostImg {
             reader.close();
             reader = null;
         } catch (Exception e) {
-            System.out.println("发送POST请求出错。" + uploadUrl);
+            System.out.println("发送POST请求出错。" + urlStr);
             e.printStackTrace();
         } finally {
             if (conn != null) {
