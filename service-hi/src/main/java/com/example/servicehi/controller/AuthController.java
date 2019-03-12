ae768fc27bac92112de8ee97742c58de9185fd67
wangqi <wangqi@wind56.com>
2019-03-12 17:20:47 +0800

    增加权限controller，删除testcontroller

---
+++ service-hi/src/main/java/com/example/servicehi/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.example.servicehi.controller;
+
+import com.example.servicehi.entity.Auth;
+import com.example.servicehi.service.AuthService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/Auth")
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class AuthController {
+    private final AuthService<Auth> authService;
+
+    @PostMapping(value = "/selectAllAuth")
+    public Object selectAllAuth(){
+        return authService.selectAll();
+    }
+}
