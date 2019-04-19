8672337e482015f3f14c00e95b37da390f396361
wangqi <wangqi@wind56.com>
2019-04-19 14:28:36 +0800

    FFXIV

--- static/vue-wap-demo/src/App.vue
+++ static/vue-wap-demo/src/App.vue
@@ -8,17 +8,6 @@
     <router-view/>
     </el-main>
     </el-container>
-    <!--<div id="main-wrapper">-->
-      <!--<div class="menu visible-md visible-lg">-->
-        <!--<ul id="menu-list">-->
-          <!--<li class="active home-menu"><a href="#">Home</a></li>-->
-          <!--<li class="about-menu"><a href="#">About Us</a></li>-->
-          <!--<li class="services-menu"><a href="#">Our Services</a></li>-->
-          <!--<li class="testimonial-menu"><a href="#">Testimonials</a></li>-->
-          <!--<li class="contact-menu"><a href="#">Contact</a></li>-->
-        <!--</ul>-->
-      <!--</div>&lt;!&ndash; /.menu &ndash;&gt;-->
-    <!--</div>-->
   </div>
 </template>
 
