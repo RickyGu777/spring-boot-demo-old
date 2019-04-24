d082afb448212c2cea9d5c50c3fdf88751c0be57
wangqi <wangqi@wind56.com>
2019-04-24 19:23:22 +0800

    打印

--- service-hi/src/main/java/com/example/servicehi/service/impl/WeChatPublicAccountServiceImpl.java
+++ service-hi/src/main/java/com/example/servicehi/service/impl/WeChatPublicAccountServiceImpl.java
@@ -3,16 +3,19 @@ package com.example.servicehi.service.impl;
 import com.example.servicehi.dao.WeChatPublicAccountDao;
 import com.example.servicehi.entity.WeChatPublicAccount;
 import com.example.servicehi.service.WeChatPublicAccountService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class WeChatPublicAccountServiceImpl<T extends WeChatPublicAccount> implements WeChatPublicAccountService<T> {
     @Autowired
     private WeChatPublicAccountDao<T> weChatPublicAccountDao;
 
     @Override
     public void insert(T t) {
+        log.info(t.toString());
         weChatPublicAccountDao.insert(t);
     }
 }
