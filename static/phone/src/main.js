47438d040a7c54a14fe3984cb1cf6b568d6cc9f6
wangqi <wangqi@wind56.com>
2019-04-25 13:11:34 +0800

    使用vux

--- static/phone/src/main.js
+++ static/phone/src/main.js
@@ -1,28 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import FastClick from 'fastclick'
-import VueRouter from 'vue-router'
 import App from './App'
-import Home from './components/HelloFromVux'
-
-Vue.use(VueRouter)
-
-const routes = [{
-  path: '/',
-  component: Home
-}]
-
-const router = new VueRouter({
-  routes
-})
-
-FastClick.attach(document.body)
+import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
+  el: '#app',
   router,
-  render: h => h(App)
-}).$mount('#app-box')
+  components: { App },
+  template: '<App/>'
+})
