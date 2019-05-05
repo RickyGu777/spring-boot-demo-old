56d912fc737b099283e280b71a3ec31ec4b22ddd
wangqi <wangqi@wind56.com>
2019-05-05 13:45:08 +0800

    删除没用的项目

--- oauth/src/main/java/com/example/oauth/util/UUIDUtil.java
+++
@@ -1,9 +0,0 @@
-package com.example.oauth.util;
-
-import java.util.UUID;
-
-public class UUIDUtil {
-    public static String createUUID(){
-        return UUID.randomUUID().toString().replace("-", "");
-    }
-}
