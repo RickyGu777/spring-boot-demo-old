4e701b53a3646b8e3b89a6e633b531de5445a003
wangqi <wangqi@wind56.com>
2019-05-09 17:18:47 +0800

    测试可变长度参数

--- wechat/src/main/java/com/example/wechat/controller/TestController.java
+++ wechat/src/main/java/com/example/wechat/controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.wechat.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(value = "/test")
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 
     @PostMapping(value = "/demo")
-    public Object demo(Object ...objects){
+    public Object demo(@RequestBody(required = false) Object ...objects){
         return objects;
     }
 }
