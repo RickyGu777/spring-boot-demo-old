2c963235de73e1a52e47bb64459f80fb9537aaeb
wangqi <wangqi@wind56.com>
2019-05-09 12:11:07 +0800

    先不做feign的测试了，先做微信公众号的功能，分离出一个单独的项目

--- wechat/src/main/java/com/example/wechat/WechatApplication.java
+++ wechat/src/main/java/com/example/wechat/WechatApplication.java
@@ -1,9 +1,15 @@
 package com.example.wechat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan("com.example.servicehi.dao")
+@EnableEurekaClient
+@EnableTransactionManagement
 public class WechatApplication {
 
     public static void main(String[] args) {
