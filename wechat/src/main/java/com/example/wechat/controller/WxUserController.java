96e022baa0383be3be3a05575ee5c1705b8a868e
wangqi <wangqi@wind56.com>
2019-05-10 17:38:37 +0800

    获取用户信息

--- wechat/src/main/java/com/example/wechat/controller/WxUserController.java
+++ wechat/src/main/java/com/example/wechat/controller/WxUserController.java
@@ -1,20 +1,22 @@
 package com.example.wechat.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@Slf4j
+@RestController
 @RequestMapping("/wx/user/{appid}")
 public class WxUserController {
     @Autowired
     private WxMpService wxService;
 
-    @RequestMapping(value = "/getUserInfo")
+    @PostMapping("/getUserInfo")
     public String user(@PathVariable String appid) throws WxErrorException {
         this.wxService.switchover(appid);
         return wxService.getAccessToken();
