947c7e44b6d4e9b0b89eaaef8f984eebcdcf7253
wangqi <wangqi@wind56.com>
2019-05-17 14:49:51 +0800

    公众号页面

---
+++ static/phone/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app" class="container-fluid">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ static/phone/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+<!--
+  <div class="row">
+      <input class="input-sm" type="text" placeholder="Inactive">
+  </div>
+-->
+  <div class="row">
+    <div class="col-md-4">.col-md-4</div>
+    <div class="col-md-4 col-md-offset-4">.col-md-4 .col-md-offset-4</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
