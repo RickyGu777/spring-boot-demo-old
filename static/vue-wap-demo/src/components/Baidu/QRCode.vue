5f14317df5b228104b33fa6f7b22a919d9d39bef
wangqi <wangqi@wind56.com>
2019-05-22 15:09:38 +0800

    修改页面

--- static/vue-wap-demo/src/components/Baidu/QRCode.vue
+++ static/vue-wap-demo/src/components/Baidu/QRCode.vue
@@ -1,6 +1,6 @@
+<!--
 <template>
   <div>
-    {{ AccessToken }}
     <el-upload
       :multiple="multipleValue"
       action="/api/Image/QRCode"
@@ -16,8 +16,6 @@
 </template>
 
 <script>
-  import {getBaiduAccessToke} from '@/request/api';
-
   export default {
     name: "Word",
     data() {
@@ -28,14 +26,7 @@
         multipleValue: true
       };
     },
-    created: function () {
-      // this.getBaiduAccessToke();
-    },
     methods: {
-      async getBaiduAccessToke() {
-        let newVar = await getBaiduAccessToke();
-        this.AccessToken = newVar.data;
-      },
       handleRemove(file, fileList) {
         console.log("handleRemove");
         console.log(file, fileList);
@@ -44,6 +35,49 @@
         console.log("handlePictureCardPreview");
         this.dialogImageUrl = file.url;
         this.dialogVisible = true;
+      }
+    }
+  }
+</script>
+-->
+
+<template>
+  <div>
+    <el-upload
+      class="upload-demo"
+      action="/api/Image/QRCode"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :on-success="handleAvatarSuccess"
+      :file-list="fileList"
+      :multiple="multipleValue"
+      list-type="picture">
+      <el-button size="small" type="primary">点击上传</el-button>
+      <!--<div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>-->
+    </el-upload>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        fileList: [],
+        multipleValue:true
+      };
+    },
+    methods: {
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePreview(file) {
+        console.log(file);
+      },
+      handleAvatarSuccess(response, file, fileList) {
+        if (file.response.code === 1) {
+          file.status = "error";
+          file.name = file.name + "\t[" + file.response.msg + "]";
+        }
       }
     }
   }
