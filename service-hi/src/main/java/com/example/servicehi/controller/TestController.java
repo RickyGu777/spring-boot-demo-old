1e8f9eb866cb881efbe037e43de83b2cfdad76f9
wangqi <wangqi@wind56.com>
2019-05-22 17:57:35 +0800

    测试insert性能

---
+++ service-hi/src/main/java/com/example/servicehi/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.example.servicehi.controller;
+
+import com.example.servicehi.entity.HotWord;
+import com.example.servicehi.service.HotWordService;
+import com.example.servicehi.util.ResponseUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/Test")
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class TestController {
+    private final HotWordService<HotWord> hotWordService;
+
+    @PostMapping(value = "/add")
+    public ResponseUtil add(@RequestBody HotWord hotWord) {
+        hotWordService.insert(hotWord);
+        return new ResponseUtil();
+    }
+}
